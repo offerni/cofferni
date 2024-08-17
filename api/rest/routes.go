d19bebf944dbe58283d4a81356cda3ec34552140
offerni <drofferni@gmail.com>
2024-08-16 22:44:44 -0700

    prefixing /api for all endpoints

--- api/rest/routes.go
+++ api/rest/routes.go
@@ -3,16 +3,18 @@ package rest
 import "github.com/go-chi/chi"
 
 func (s *Server) routes() {
-	// orders group
-	s.Router.Route("/orders", func(r chi.Router) {
-		r.Get("/", s.ListOrders)
-		r.Post("/", s.CreateOrder)
-		r.Patch("/", s.UpdateOrder)
-	})
+	s.Router.Route("/api", func(r chi.Router) {
 
-	// items group
-	s.Router.Route("/items", func(r chi.Router) {
-		r.Get("/", s.ListItems)
-	})
+		// orders group
+		r.Route("/orders", func(r chi.Router) {
+			r.Get("/", s.ListOrders)
+			r.Post("/", s.CreateOrder)
+			r.Patch("/", s.UpdateOrder)
+		})
 
+		// items group
+		r.Route("/items", func(r chi.Router) {
+			r.Get("/", s.ListItems)
+		})
+	})
 }
