20d36fa5e0d18c9381cf4ca7f68361e4b99f6e0a
offerni <drofferni@gmail.com>
2024-08-17 00:11:58 -0700

    adding tailwind colours

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -1,20 +1,35 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+        './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    ],
+    theme: {
+        extend: {
+            primary: {
+                light: '#A86854',
+                DEFAULT: '#884A39',
+            },
+            secondary: {
+                light: '#D29D73',
+                DEFAULT: '#C38154',
+            },
+            tertiary: {
+                light: '#FFD7A1',
+                DEFAULT: '#FFC26F',
+            },
+            neutral: {
+                light: '#FBF1D6',
+                DEFAULT: '#F9E0BB',
+            },
+            destructive: {
+                light: '#8B4C52',
+                DEFAULT: '#6D2932',
+            },
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
 export default config;
